d1510e7ef514205745dfcc9dc56a3257f5bfa8e1
trushar.mistry <trushar.mistry@hiddenbrains.in>
2023-01-20 14:36:11 +0530

    change import name

--- lib/circular_stack.dart
+++
@@ -1,6 +0,0 @@
-library circular_progress_stack;
-
-export 'src/Animated_Circle_Painter/animated_circle.dart';
-export 'src/circle_progress.dart';
-export 'src/utils.dart';
-export 'src/values_model.dart';
