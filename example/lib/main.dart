0e593d6aef19420440902db23c73f53f9d4f6f48
trushar.mistry <trushar.mistry@hiddenbrains.in>
2023-01-20 14:34:12 +0530

    after change

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:circular_progress_stack/circularprogressstack.dart';
+import 'package:circular_progress_stack/circular_stack.dart';
 import 'package:flutter/material.dart';
 
 void main() {
